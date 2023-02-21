8f41cf597d240d3e7b898e9ac3a22e8b463482d4
Jonetta <jonetta.pek@fdmgroup.com>
2023-02-21 08:33:16 +0800

    Registration form data will be saved as states in a Doctor Bean and the Entity will be saved as a record in the database

---
+++ src/main/java/com/fdmgroup/appointmentbooking/model/Doctor.java
@@ -0,0 +1,119 @@
+package com.fdmgroup.appointmentbooking.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "doctor")
+@Component
+public class Doctor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "doctor_id")
+	private long doctorID;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email", unique = true)
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "specialty")
+	private String specialty;
+	
+	@OneToMany(mappedBy = "doctor")
+	private List<Appointment> appointments = new ArrayList<Appointment>();
+	
+	public Doctor() {
+		super();
+		
+	}
+	
+	public Doctor(int doctorID, String firstName, String lastName, String email, String password, String specialty) {
+		super();
+		this.doctorID = doctorID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.specialty = specialty;
+	}
+	
+	public long getDoctorID() {
+		return this.doctorID;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getSpecialty() {
+		return this.specialty;
+	}
+	
+	public List<Appointment> getAppointments() {
+		return this.appointments;
+	}
+	
+	public void setDoctorID(int doctorID) {
+		this.doctorID = doctorID;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	
+	public void setAppointments(List<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+	
+	public void addAppointment(Appointment appointment) {
+		appointments.add(appointment);
+	}
+	
+}
